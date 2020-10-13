af2029d7f5b64ed552c54a08b5098b0e58200831
Bhushan Borkar <bhborkar@in.ibm.com>
2020-10-13 08:40:14 +0100

    initial commit

---
+++ plugin/library/src/main/java/com/bhborkar/ide/library/App.java
@@ -0,0 +1,12 @@
+package com.bhborkar.ide.library;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String get() {
+		return "From library";
+	}
+}
---
+++ plugin/osgi-library/src/com/bhborkar/ide/osgi/OSGIDependency.java
@@ -0,0 +1,11 @@
+package com.bhborkar.ide.osgi;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class OSGIDependency {
+	
+	public String get() {
+		return StringUtils.upperCase("hello from osgi");
+	}
+
+}
